eb2a91eda2b227b10fc26e5f1162860496efb472
blueyleader <blueyleader@gmail.com>
2015-12-08 19:38:07 -0500

    day 9 stubs
    added stubs for day 9 for fast deployment

---
+++ src/com/blueyleader/adventofcode/Day9_2.java
@@ -0,0 +1,16 @@
+package com.blueyleader.adventofcode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day9_2 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		String input;
+		File file = new File("day9.txt");
+		Scanner scan = new Scanner(file);
+
+	}
+
+}
