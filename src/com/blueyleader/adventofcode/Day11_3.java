da6406ae8e71eba05f6897b343b506c29a202f6b
blueyleader <blueyleader@gmail.com>
2015-12-11 17:49:31 -0500

    Day11_3
    opt a little more

--- src/com/blueyleader/adventofcode/Day11_3.java
+++ src/com/blueyleader/adventofcode/Day11_3.java
@@ -18,7 +18,7 @@ public class Day11_3 {
 		while(flag){
 			c=0;
 			seq=false;
-			if(input.indexOf('i')==-1 && input.indexOf('0')==-1 && input.indexOf('l')==-1){
+			if(input.indexOf('i')==-1 && input.indexOf('o')==-1 && input.indexOf('l')==-1){
 				for(int x=0;x<input.length()-2;x++){
 					if((input.charAt(x)+1==(input.charAt(x+1))) && (input.charAt(x)+2==(input.charAt(x+2)))){
 						seq=true;
@@ -48,9 +48,25 @@ public class Day11_3 {
 				boolean change=true;
 				for(int x=input.length()-1;x>-1;x--){
 					if(change){
-						if(input.charAt(x)=='z'){
+						char b =input.charAt(x);
+						if(b=='z'){
 							char t='a';
 							input=input.substring(0,x)+t+input.substring(x+1,input.length());
+						}
+						else if(b=='h'){
+							char t='j';
+							input=input.substring(0,x)+t+input.substring(x+1,input.length());
+							change=false;
+						}
+						else if(b=='n'){
+							char t='p';
+							input=input.substring(0,x)+t+input.substring(x+1,input.length());
+							change=false;
+						}
+						else if(b=='k'){
+							char t='m';
+							input=input.substring(0,x)+t+input.substring(x+1,input.length());
+							change=false;
 						}
 						else{
 							char t=(char) (input.charAt(x)+1);
