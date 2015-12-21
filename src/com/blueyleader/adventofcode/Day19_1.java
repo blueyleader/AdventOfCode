932bd24c0c99d5f9f3801d1275e0ee008bc324f9
blueyleader <blueyleader@gmail.com>
2015-12-21 10:47:05 -0500

    Day 19 correction
    fixed part numbers of day 19

--- src/com/blueyleader/adventofcode/Day19_1.java
+++ src/com/blueyleader/adventofcode/Day19_1.java
@@ -9,22 +9,22 @@ import java.util.Scanner;
 
 public class Day19_1 {
 
-	static int max,counter;
+	static int max,c;
 	static ArrayList<String> answers;
-	static HashMap<String,String> map;
-	static String out;
+	static HashMap<String,ArrayList<String>> map;
 	public static void main(String[] args) throws FileNotFoundException {
 		String input="";
 		File file = new File("day19.txt");
 		Scanner scan = new Scanner(file);
-		map = new HashMap<String, String>();
+		boolean [][] data = new boolean[100][100];
+		map = new HashMap<String, ArrayList<String>>();
 		String temp[];
 		ArrayList<String> atemp;
 		answers = new ArrayList<String>();
 		boolean flag=true;
 		int count=0;
-		max=Integer.MAX_VALUE;
-
+		max=150;
+		c=0;
 		while(scan.hasNextLine()){
 			input=scan.nextLine();
 			if(input.equals("")){
@@ -32,41 +32,34 @@ public class Day19_1 {
 			}
 			if(flag){
 				temp=input.split(" ");
-				map.put(temp[2], temp[0]);
+				if(!map.containsKey(temp[0])){
+					map.put(temp[0], new ArrayList<String>());
+				}
+				atemp=map.get(temp[0]);
+				atemp.add(temp[2]);
+				map.put(temp[0],atemp);
 			}
 		count++;
 		}
-		out=input;
-		rec(input,0);
 		
-		System.out.println(max);
-		
-	}
-	
-	
-	/*public static void rec2(String in,int step){
-		if(in.length()>out.length() || step>max)
-			return;
-		else if(in.equals(out)){
-			if(step<max){
-				max=step;
-			}
-		}
-		
-		char c=in.charAt(0);
-		String cur=""+c;
-		ArrayList<String> atemp;
-		for(int x=1;x<in.length();x++){
-			c=in.charAt(x);
-			System.out.println(in);
+		String cur="";
+		String out="";
+		ArrayList<String> v= new ArrayList<String>();
+		//answers.add("");
+		char c;
+		c=input.charAt(0);
+		cur=""+c;
+		for(int x=1;x<input.length();x++){
+			c=input.charAt(x);
+			//System.out.println(x+ ": "+answers.size());
 			if((c>='A' && c<='Z') || c=='e'){
 				atemp=map.get(cur);
 				if(atemp!=null){
 					for(int z=0;z<atemp.size();z++){
-						String v1=in.substring(0,x-cur.length());
+						String v1=input.substring(0,x-cur.length());
 						String v2=atemp.get(z);
-						String v3=in.substring(x);
-						rec(v1+v2+v3,step+1);
+						String v3=input.substring(x);
+						answers.add(v1+v2+v3);
 					}
 				}				
 				cur=""+c;
@@ -75,48 +68,26 @@ public class Day19_1 {
 				cur+=c;
 			}
 		}
-		
 		atemp=map.get(cur);
 		if(atemp!=null){
 			for(int z=0;z<atemp.size();z++){
-				String v1=in.substring(0,in.length()-cur.length());
+				String v1=input.substring(0,input.length()-cur.length());
 				String v2=atemp.get(z);
-				rec(v1+v2,step+1);
+				answers.add(v1+v2);
 			}
-		}	
-	}*/
-	
-	public static void rec(String in , int step){
-		if(step>=max)
-			return;
-		else if(in.equals("e")){
-			if(step<max){
-				max=step;
+		}				
+		
+		ArrayList<String> answers2 = new ArrayList<String>();
+		count=0;
+		for(int x=0;x<answers.size();x++){
+			flag=false;
+			if(!answers2.contains(answers.get(x))){
+				answers2.add(answers.get(x));
+				count++;
 			}
 		}
-		/*char c=in.charAt(0);
-		String cur =""+c;
-		String rep;
-		ArrayList<String> atemp;*/
-		System.out.println(in);
-		for(int x=0;x<in.length();x++){
-			
-			for (HashMap.Entry<String, String> entry : map.entrySet())
-			{
-				if(in.indexOf(entry.getKey())==x){
-					String v1=in.substring(0,x);
-					String v2=in.substring(x+entry.getKey().length());
-					rec(v1+entry.getValue()+v2,step+1);	
-				}
-			}		
-		}
 		
-		/*rep=map.get(cur);
-		if(rep!=null){
-			String v1=in.substring(0,in.length()-cur.length());
-			rec(v1+rep,step+1);
-		}	*/
-		
+		System.out.println(count);
 		
 	}
 	
--- src/com/blueyleader/adventofcode/Day19_2.java
+++ src/com/blueyleader/adventofcode/Day19_2.java
@@ -3,93 +3,44 @@ package com.blueyleader.adventofcode;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Day19_2 {
 
-	static int max,c;
-	static ArrayList<String> answers;
-	static HashMap<String,ArrayList<String>> map;
-	public static void main(String[] args) throws FileNotFoundException {
+    public static String replace(String s, String in, String out, int position) {
+        return s.substring(0, position) + out + s.substring(position + in.length());
+    }
+    public static void main(String[] args) throws FileNotFoundException {
 		String input="";
 		File file = new File("day19.txt");
 		Scanner scan = new Scanner(file);
-		boolean [][] data = new boolean[100][100];
-		map = new HashMap<String, ArrayList<String>>();
-		String temp[];
-		ArrayList<String> atemp;
-		answers = new ArrayList<String>();
+		
+		ArrayList<String[]> array = new ArrayList<String[]>();
 		boolean flag=true;
-		int count=0;
-		max=150;
-		c=0;
 		while(scan.hasNextLine()){
 			input=scan.nextLine();
 			if(input.equals("")){
 				flag=false;
 			}
 			if(flag){
-				temp=input.split(" ");
-				if(!map.containsKey(temp[0])){
-					map.put(temp[0], new ArrayList<String>());
-				}
-				atemp=map.get(temp[0]);
-				atemp.add(temp[2]);
-				map.put(temp[0],atemp);
-			}
-		count++;
-		}
-		
-		String cur="";
-		String out="";
-		ArrayList<String> v= new ArrayList<String>();
-		//answers.add("");
-		char c;
-		c=input.charAt(0);
-		cur=""+c;
-		for(int x=1;x<input.length();x++){
-			c=input.charAt(x);
-			//System.out.println(x+ ": "+answers.size());
-			if((c>='A' && c<='Z') || c=='e'){
-				atemp=map.get(cur);
-				if(atemp!=null){
-					for(int z=0;z<atemp.size();z++){
-						String v1=input.substring(0,x-cur.length());
-						String v2=atemp.get(z);
-						String v3=input.substring(x);
-						answers.add(v1+v2+v3);
-					}
-				}				
-				cur=""+c;
-			}
-			else{
-				cur+=c;
+				String[] temp=input.split(" ");
+				array.add(new String[]{temp[0],temp[2]});
 			}
 		}
-		atemp=map.get(cur);
-		if(atemp!=null){
-			for(int z=0;z<atemp.size();z++){
-				String v1=input.substring(0,input.length()-cur.length());
-				String v2=atemp.get(z);
-				answers.add(v1+v2);
-			}
-		}				
-		
-		ArrayList<String> answers2 = new ArrayList<String>();
-		count=0;
-		for(int x=0;x<answers.size();x++){
-			flag=false;
-			if(!answers2.contains(answers.get(x))){
-				answers2.add(answers.get(x));
-				count++;
-			}
-		}
-		
-		System.out.println(count);
-		
-	}
-	
-}
+ 
+        List<String> output = new ArrayList<>();
 
+        int count2 = 0;
+        while(!input.equals("e")) {
+            for (String[] each : array) {
+                if (input.contains(each[1])) {
+                    input = replace(input, each[1], each[0], input.lastIndexOf(each[1]));
+                    count2++;
+                }
+            }
+        }
+        System.out.println(count2);
+    }
+}
