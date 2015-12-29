1bb26289a2c9448a42f02decf22a04f503c10747
blueyleader <blueyleader@gmail.com>
2015-12-28 23:06:57 -0500

    day 10 small update

--- src/com/blueyleader/adventofcode/Day10_0.java
+++ src/com/blueyleader/adventofcode/Day10_0.java
@@ -2,8 +2,6 @@ package com.blueyleader.adventofcode;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Day10_0 {
